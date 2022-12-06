0d1fbaeef0e1dcb953f092a6d659431c65358cb6
Alexander Megadrive <alexmihailov91@gmail.com>
2022-12-06 17:13:58 +0300

    feat: update API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '231e4c7b702d4c358cee0ba6f681ab9f', // получите свой ключ https://newsapi.org/
         });
     }
 }

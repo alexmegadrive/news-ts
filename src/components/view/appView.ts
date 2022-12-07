da7ae1e6af26a2ac1eb18cbac5a32795f7b23071
Alexander Megadrive <alexmihailov91@gmail.com>
2022-12-07 20:20:25 +0300

    feat: add types in appView

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,21 +1,48 @@
 import News from './news/news';
 import Sources from './sources/sources';
 
+export interface INewsSourcesData {
+    sources: Array<INewsSourceItem>;
+    status?: string;
+}
+interface INewsSourceItem {
+    [key: string]: string;
+}
+export interface INewsSourceArchive {
+    articles: Array<INewsArticle>;
+    status: string;
+    totalResults: number;
+}
+export interface INewsArticle {
+    author:string;
+    content:string;
+    description:string;
+    publishedAt:string;
+    source:INewsArticleSource;
+    title:string;
+    url:string;
+    urlToImage:string;
+}
+interface INewsArticleSource {
+    id: string;
+    name: string;
+}
+
 export class AppView {
-    news:any;
-    sources:any;
-    
+    news: News;
+    sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data:INewsSourceArchive) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: INewsSourcesData) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }

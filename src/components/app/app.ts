bcda71de11de17a44a26bbcac310264364d06d45
Alexander Megadrive <alexmihailov91@gmail.com>
2022-12-07 20:20:08 +0300

    feat: add types for app.ts

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,9 +1,9 @@
 import AppController from '../controller/controller';
-import { AppView } from '../view/appView';
+import { AppView, INewsSourcesData, INewsSourceArchive } from '../view/appView';
 
 class App {
-    controller:any;
-    view:any;
+  private  controller:AppController;
+   private view:AppView;
     
     constructor() {
         this.controller = new AppController();
@@ -11,10 +11,10 @@ class App {
     }
 
     start() {
-        document
-            .querySelector('.sources')!
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+       const sourcesContainer = document.querySelector('.sources') as HTMLElement;
+       if (!sourcesContainer) throw new Error('Sources Container not exists')
+        sourcesContainer.addEventListener('click', (e:Event) => this.controller.getNews(e, (data:INewsSourceArchive) => this.view.drawNews(data)));
+        this.controller.getSources((data:INewsSourcesData) => this.view.drawSources(data));
     }
 }
 
